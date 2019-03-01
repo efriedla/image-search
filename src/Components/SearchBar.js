38df947815f5cb8427ff4ca214f030f517f8e8bf
efriedla <efriedla20852@gmail.com>
2019-03-01 11:42:38 -0800

    form sumbit is bound

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = {term: ''}
-   
+    state = {term: ''};
+
+   onFormSubmit = (event) =>{
+    event.preventDefault()
+    console.log(this.state.term);
+   };
     render(){
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" 
@@ -16,7 +20,7 @@ class SearchBar extends React.Component {
                     </div>
                 </form>
             </div>
-        )
-    }
-}
+        );
+    };
+};
 export default SearchBar;

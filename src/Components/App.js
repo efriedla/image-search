9e7e866a0698e4e69beb6ad5810df1513a9c4f63
efriedla <efriedla20852@gmail.com>
2019-03-04 11:26:19 -0800

    added Key's to hide authorization

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
+var {ClientIDUnsplash} = require('./Keys');
 
 class App extends React.Component {
-    onSearchSubmit(term) {
+    
+    async onSearchSubmit(term) {
         console.log(term);
-        axios.get('https://api.unsplash.com/search/photos', {
+       const response = await axios.get('https://api.unsplash.com/search/photos', {
             params : { query: term },
             headers : {
-                Authorization: ''
+                Authorization: `Client-ID ${ClientIDUnsplash}`
             }
         });
+        console.log(response.data.results);
     }
     render(){
         return(
             <div className="ui container">
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+               
             </div>
         )
     }

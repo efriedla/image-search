c553b53fccede27401c321124d9b85443f76de1c
efriedla <efriedla20852@gmail.com>
2019-03-04 14:06:01 -0800

    created a new component for images

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './List.css';
+import ImageCard from './ImageCard';
 
 const List = (props) => {
     console.log(props.images);
    const images = props.images.map((image) => {
-        return <img key={image.id} alt={image.description} src={image.urls.regular}/>
+        return <ImageCard key={image.id} image={image} />
     });
     return(
-        <div>{images}</div>
+        <div className="image-list">
+        {images}
+        </div>
+
     )
 }
 

e4d8319c77aaaced01bca58df241086b940bbb7c
efriedla <efriedla20852@gmail.com>
2019-05-05 15:26:54 -0700

    comments generating with faker

--- src/Components/Comments/CommentDetails.js
+++ src/Components/Comments/CommentDetails.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetails = (props) => {
-    
+    console.log(props.day)
     return(
         <div className="comment">
                 <a className="avatar" herf="/">
@@ -11,14 +10,14 @@ const CommentDetails = (props) => {
                 <div className="content">
                 <a className="author">{props.author}</a>
                 <div className="metadata">
-                    <div className="date">2 days ago</div>
+                    <div className="date">{props.day} days ago</div>
                     <div className="rating">
                     <i className="star icon"></i>
                     5 Faves.
                     </div>
                 </div>
                 <div className="text">
-                    Hey guys, I hope this example comment is helping you read this documentation.
+                    {props.comment}
                 </div>
                 </div>
             </div>
--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -5,8 +5,27 @@ import faker from 'faker';
 const Comments = () => {
     return(
         <div className="ui comments">
-            <CommentDetails author = "Stevie Feliciano" image = {faker.image.avatar()} />
-            <CommentDetails author = "Kevin Fano" image = {faker.image.avatar()}/>
+            <CommentDetails 
+                author = {faker.name.findName()} 
+                image = {faker.image.avatar()}
+                comment = {faker.lorem.paragraph()} 
+                day = {faker.random.number(2)}
+                review = {faker.random.number(5)}
+            />
+            <CommentDetails 
+                author = {faker.name.findName()} 
+                image = {faker.image.avatar()}
+                comment = {faker.lorem.sentences()} 
+                day = {faker.random.number(10)}
+                review = {faker.random.number(5)}
+            />
+            <CommentDetails 
+                author = {faker.name.findName()} 
+                image = {faker.image.avatar()}
+                comment = {faker.lorem.lines()} 
+                day = {faker.random.number(20)}
+                review = {faker.random.number(5)}
+            />
         </div>
     )
 }
